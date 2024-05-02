5aad0568597b7dcb54cc0072946d3b08e6393200
farhaankaleem <farhaan.kaleem@gmail.com>
2024-05-02 15:13:37 +0100

    Refactor to allow for new filtering hook, and add the hook.

--- src/components/movieList/index.tsx
+++ src/components/movieList/index.tsx
@@ -3,7 +3,7 @@ import Movie from "../movieCard/";
 import Grid from "@mui/material/Grid";
 import { BaseMovieList } from "../../types/interfaces";
 
-const MovieList: React.FC<BaseMovieList> = (props) => {
+const MovieList: React.FC<MovieListProps> = (props) => {
   const movies=props.movies;
   let movieCards = movies.map((m) => (
     <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
